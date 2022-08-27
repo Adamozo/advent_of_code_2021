2a3f51224ff8fd41e41157f942dfe69417719028
Adamozo <naworsk.adam@gmail.com>
2022-08-27 23:18:21 +0200

    pr changes

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,5 +2,5 @@ use advent_of_code_2021::day3::Day3;
 use aoc_utils::DaySolver;
 
 fn main() {
-    Day3::timeit_solve_default_file();
+    Day3::timeit_solution();
 }
--- src/day3.rs
+++ src/day3.rs
@@ -8,29 +8,38 @@ impl DaySolver for Day3 {
 
     const INFO: DayInfo = DayInfo::with_day_and_file("day3", "data/day3.txt");
 
-    fn solution(_s: &str) -> anyhow::Result<<Self as DaySolver>::Output> {
-        let bit_occurs = _s
-            .lines()
-            .fold(vec![vec![0; 2]; 5], |mut bit_occurs, line| {
-                for (index, value) in line.chars().enumerate() {
-                    bit_occurs[index][value as usize - '0' as usize] += 1; // because ascii value is returned
-                }
+    fn solution(s: &str) -> anyhow::Result<<Self as DaySolver>::Output> {
+        const COUNT_OF_DIGITS: usize = 5;
 
-                bit_occurs
-            });
+        let bit_occurs =
+            s.lines()
+                .fold(vec![vec![0; 2]; COUNT_OF_DIGITS], |mut bit_occurs, line| {
+                    for (index, value) in line.bytes().enumerate() {
+                        let value_index = (value - b'0') as usize; // because ascii value is returned
+                        bit_occurs[index][value_index] += 1;
+                    }
 
-        let most_common = bit_occurs
+                    bit_occurs
+                });
+
+        let gamma_rate = bit_occurs
             .iter()
             .enumerate()
             .fold(0, |res, (index, values)| {
-                if values[1] > values[0] { // values[1] number of 1 occurs, values[0] number of 0 occurs
-                    return res | 1 << (4 - index);
+                // values[0] - number of 0 occurs
+                // values[1] - number of 1 occurs,
+                // the max. value of the 'index' variable -> 4
+                if values[1] > values[0] {
+                    res | 1 << (4 - index)
+                } else {
+                    res
                 }
-
-                res
             });
 
-        Ok(most_common * ((!most_common) - 224)) // 224 <=> 11100000
+        let bit_mask = (1 << COUNT_OF_DIGITS) - 1;
+        let epsilon_rate = !gamma_rate & bit_mask;
+
+        Ok(gamma_rate * epsilon_rate)
     }
 }
 
