2007b63ae227f5b795b19011dfad8b31eea92f4f
Adamozo <naworsk.adam@gmail.com>
2022-08-28 02:17:25 +0200

    pr changes

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,7 @@
 use anyhow::*;
 use aoc_utils::DayInfo;
 use aoc_utils::DaySolver;
+use fnv::FnvHashMap as HashMap;
 
 pub struct Day4;
 
@@ -9,26 +10,23 @@ impl DaySolver for Day4 {
 
     const INFO: DayInfo = DayInfo::with_day_and_file("day4", "data/day4.txt");
 
-    fn solution(_s: &str) -> anyhow::Result<<Self as DaySolver>::Output> {
-        let mut input = _s.split("\n\n");
+    fn solution(s: &str) -> anyhow::Result<<Self as DaySolver>::Output> {
+        let (called_numbers, boards) = s.split_once("\n\n").unwrap();
 
-        let bingo_numbers: Vec<usize> = input
-            .next()
-            .unwrap()
+        let called_numbers: Vec<usize> = called_numbers
             .split(',')
             .map(|num| num.parse::<usize>().unwrap())
             .collect();
 
-        let max_value = bingo_numbers.iter().max().unwrap();
+        let mut boards: Vec<Board> = boards
+            .split("\n\n")
+            .map(Board::new)
+            .collect();
 
-        let mut boards: Vec<Board> = input.map(|board| Board::new(board, max_value)).collect();
-
-        for current_number in bingo_numbers {
-            for board in &mut boards {
-                board.mark_number(&current_number);
-
-                if board.is_solved(&current_number) {
-                    return Ok(board.unmarked_sum() * current_number);
+        for current_number in called_numbers {
+            for board in boards.iter_mut() {
+                if let Some(output) = board.call_number(&current_number) {
+                    return Ok(output);
                 }
             }
         }
@@ -44,17 +42,18 @@ enum FieldState {
 }
 
 type Location = (usize, usize);
+type Number = usize;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Board {
     fields_states:   Vec<Vec<FieldState>>,
-    number_location: Vec<Option<Location>>,
+    number_location: HashMap<Number, Location>,
 }
 
 impl Board {
-    fn new(input: &str, max_value: &usize) -> Self {
+    fn new(input: &str) -> Self {
         let fields_states = vec![vec![FieldState::Unmarked; 5]; 5];
-        let mut number_location: Vec<Option<Location>> = vec![None; max_value + 1];
+        let mut number_location = HashMap::default();
 
         for (row, line) in input.lines().enumerate() {
             for (column, number) in line
@@ -63,7 +62,7 @@ impl Board {
                 .map(|num| num.parse::<usize>().unwrap())
                 .enumerate()
             {
-                number_location[number] = Some((row, column));
+                number_location.insert(number, (row, column));
             }
         }
 
@@ -73,37 +72,34 @@ impl Board {
         }
     }
 
-    fn mark_number(&mut self, number: &usize) {
-        if let Some((row, column)) = self.number_location[*number] {
-            self.fields_states[row][column] = FieldState::Marked;
-        }
-    }
+    fn call_number(&mut self, number: &usize) -> Option<usize> {
+        if let Some((row, column)) = self.number_location.get(number) {
+            self.fields_states[*row][*column] = FieldState::Marked;
 
-    fn is_solved(&self, number: &usize) -> bool {
-        if let Some((row, column)) = self.number_location[*number] {
-            return !self.fields_states[row]
-                .iter()
-                .any(|field| *field == FieldState::Unmarked)
-                || !(0..self.fields_states.len())
-                    .any(|row| self.fields_states[row][column] == FieldState::Unmarked);
+            let is_solved = |row: &usize, column: &usize| {
+                !self.fields_states[*row]
+                    .iter()
+                    .any(|field| *field == FieldState::Unmarked)
+                    || !(0..self.fields_states.len())
+                        .any(|row| self.fields_states[row][*column] == FieldState::Unmarked)
+            };
+
+            if is_solved(row, column) {
+                let res = self.unmarked_sum();
+                return Some(res * number);
+            }
         }
 
-        false
+        None
     }
 
     fn unmarked_sum(&self) -> usize {
         self.number_location
             .iter()
-            .enumerate()
-            .filter(|(_number, location)| location.is_some())
-            .map(|(number, location)| {
-                let (row, column) = location.unwrap();
-                (number, row, column)
-            })
-            .filter(|(_number, row, column)| {
+            .filter(|(_number, (row, column))| {
                 self.fields_states[*row][*column] == FieldState::Unmarked
             })
-            .map(|(number, _, _)| number)
+            .map(|(number, _)| number)
             .sum()
     }
 }
@@ -116,169 +112,5 @@ mod tests {
     #[test]
     fn data_from_default_file() {
         assert_eq!(Day4::solve_default_file().unwrap(), 4512)
-    }
-
-    #[test]
-    fn day4_board_new() {
-        let input = r#"22 13 17 11  0
-8  2 23  4 24
-21  9 14 16  7
-6 10  3 18  5
-1 12 20 15 19"#;
-
-        let number_location = vec![
-            Some((0, 4)),
-            Some((4, 0)),
-            Some((1, 1)),
-            Some((3, 2)),
-            Some((1, 3)),
-            Some((3, 4)),
-            Some((3, 0)),
-            Some((2, 4)),
-            Some((1, 0)),
-            Some((2, 1)),
-            Some((3, 1)),
-            Some((0, 3)),
-            Some((4, 1)),
-            Some((0, 1)),
-            Some((2, 2)),
-            Some((4, 3)),
-            Some((2, 3)),
-            Some((0, 2)),
-            Some((3, 3)),
-            Some((4, 4)),
-            Some((4, 2)),
-            Some((2, 0)),
-            Some((0, 0)),
-            Some((1, 2)),
-            Some((1, 4)),
-            None,
-            None,
-        ];
-        let fields_states = vec![vec![FieldState::Unmarked; 5]; 5];
-
-        assert_eq!(
-            Board::new(input, &26),
-            Board {
-                fields_states,
-                number_location
-            }
-        )
-    }
-
-    #[test]
-    fn day4_board_mark_number() {
-        let input = r#"22 13 17 11  0
-8  2 23  4 24
-21  9 14 16  7
-6 10  3 18  5
-1 12 20 15 19"#;
-
-        let mut board = Board::new(input, &26);
-        board.mark_number(&26);
-        board.mark_number(&22);
-
-        assert_eq!(board.fields_states[0][0], FieldState::Marked)
-    }
-
-    #[test]
-    fn day4_board_is_solved_false() {
-        let input = r#"22 13 17 11  0
-8  2 23  4 24
-21  9 14 16  7
-6 10  3 18  5
-1 12 20 15 19"#;
-
-        let board = Board::new(input, &26);
-
-        assert_eq!(board.is_solved(&11), false)
-    }
-
-    #[test]
-    fn day4_board_is_solved_true() {
-        let number_location = vec![
-            Some((0, 4)),
-            Some((4, 0)),
-            Some((1, 1)),
-            Some((3, 2)),
-            Some((1, 3)),
-            Some((3, 4)),
-            Some((3, 0)),
-            Some((2, 4)),
-            Some((1, 0)),
-            Some((2, 1)),
-            Some((3, 1)),
-            Some((0, 3)),
-            Some((4, 1)),
-            Some((0, 1)),
-            Some((2, 2)),
-            Some((4, 3)),
-            Some((2, 3)),
-            Some((0, 2)),
-            Some((3, 3)),
-            Some((4, 4)),
-            Some((4, 2)),
-            Some((2, 0)),
-            Some((0, 0)),
-            Some((1, 2)),
-            Some((1, 4)),
-            None,
-            None,
-        ];
-        let fields_states = vec![vec![FieldState::Marked; 5]; 5];
-
-        let board = Board {
-            number_location,
-            fields_states,
-        };
-
-        assert_eq!(board.is_solved(&1), true)
-    }
-
-    #[test]
-    fn day4_board_unmarked_sum() {
-        let number_location = vec![
-            Some((0, 4)),
-            Some((4, 0)),
-            Some((1, 1)),
-            Some((3, 2)),
-            Some((1, 3)),
-            Some((3, 4)),
-            Some((3, 0)),
-            Some((2, 4)),
-            Some((1, 0)),
-            Some((2, 1)),
-            Some((3, 1)),
-            Some((0, 3)),
-            Some((4, 1)),
-            Some((0, 1)),
-            Some((2, 2)),
-            Some((4, 3)),
-            Some((2, 3)),
-            Some((0, 2)),
-            Some((3, 3)),
-            Some((4, 4)),
-            Some((4, 2)),
-            Some((2, 0)),
-            Some((0, 0)),
-            Some((1, 2)),
-            Some((1, 4)),
-            None,
-            None,
-        ];
-        let fields_states = vec![vec![FieldState::Unmarked; 5]; 5];
-
-        let mut board = Board {
-            number_location,
-            fields_states,
-        };
-
-        board.fields_states[0][0] = FieldState::Marked;
-        board.fields_states[1][1] = FieldState::Marked;
-        board.fields_states[2][2] = FieldState::Marked;
-        board.fields_states[3][3] = FieldState::Marked;
-        board.fields_states[4][4] = FieldState::Marked;
-
-        assert_eq!(board.unmarked_sum(), 225)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod day4;
+pub mod day4_no_searching;
 pub mod dayx;
