ba7029e1fdb8cf46eb7e974134ef6759496cc629
Adamozo <naworsk.adam@gmail.com>
2022-09-26 15:01:50 +0200

    separate parsing function, grid from aoc_utils

--- src/day13b.rs
+++ src/day13b.rs
@@ -1,12 +1,13 @@
 use aoc_utils::DayInfo;
 use aoc_utils::DaySolver;
-use fnv::FnvHashSet as HashSet;
+use aoc_utils::Grid;
+use itertools::Itertools;
 
-pub struct Day13;
+pub struct Day13b;
 
 type Point = (i8, i8);
 
-impl DaySolver for Day13 {
+impl DaySolver for Day13b {
     type Output = usize;
 
     const INFO: DayInfo = DayInfo::with_day_and_file("day13", "data/day13.txt");
@@ -14,7 +15,7 @@ impl DaySolver for Day13 {
     fn solution(s: &str) -> anyhow::Result<<Self as DaySolver>::Output> {
         let (coordinates, folds) = s.split_once("\n\n").unwrap();
 
-        let mut dots: HashSet<Point> = parse_input_coordinates(coordinates);
+        let mut dots: Grid<Point> = parse_input_coordinates(coordinates);
 
         for fold_line in folds
             .lines()
@@ -25,27 +26,25 @@ impl DaySolver for Day13 {
             .filter(|(_line, axis)| *axis == 'y')
             .map(|(line, _)| line.parse::<u8>().unwrap())
         {
-            dots = dots
-                .iter()
-                .fold(HashSet::default(), |mut dots: HashSet<Point>, point| {
-                    let new_point = convert_point_horizontal(fold_line as i8, *point);
-                    let _unused = dots.insert(new_point);
-                    dots
-                });
+            for (_position, point) in dots.enumerate_mut() {
+                *point = convert_point_horizontal(fold_line as i8, *point);
+            }
         }
 
-        Ok(dots.len())
+        Ok(dots.values().unique().count())
     }
 }
 
-fn parse_input_coordinates(s: &str) -> HashSet<Point> {
-    s
-    .lines()
-    .map(|line| {
+fn parse_input_coordinates(s: &str) -> Grid<Point> {
+    let mut result: Grid<Point> = Grid::new(1, s.lines().count());
+
+    for (idx, line) in s.lines().enumerate() {
         let (x, y) = line.split_once(',').unwrap();
-        (x.parse::<i8>().unwrap(), y.parse::<i8>().unwrap())
-    })
-    .collect()
+        *result.get_mut_with_idx(idx).unwrap() =
+            (x.parse::<i8>().unwrap(), y.parse::<i8>().unwrap());
+    }
+
+    result
 }
 
 fn convert_point_horizontal(symmetry_line: i8, point: Point) -> Point {
@@ -63,6 +62,6 @@ mod tests {
 
     #[test]
     fn data_from_default_file() {
-        assert_eq!(Day13::solve_default_file().unwrap(), 17)
+        assert_eq!(Day13b::solve_default_file().unwrap(), 17)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,4 +11,5 @@ pub mod day9;
 pub mod day10;
 pub mod day11;
 pub mod day13;
+pub mod day13b;
 pub mod dayx;
